45ec61e311233915c8e6c51588b2e2e5d2229bcc
Glen Pike <glen.pike@olioex.com>
2025-12-13 17:41:01 +0000

    Fixing types for name clashes and use types in Note component
    Remove cruft from types

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useVolcaStore } from '../../stores/useVolcaStore'
 import { noteNumberToName, noteNumberToOctave, musicalNotes, noteToMidi } from '../../utils/MidiNotes'
 import './Note.css'
+import { SequenceInfo, StepInfo, NoteInfo } from '../../types/volca'
 
 interface NoteProps {
   noteId: number;
@@ -12,11 +13,11 @@ interface NoteProps {
 }
 
 const Note = ({ noteId, sequenceId, stepId, on, motionData }: NoteProps) => {
-  const note = useVolcaStore((state) =>
+  const note: NoteInfo = useVolcaStore((state) =>
     state.sequences
-      .find((seq) => seq.programNumber === sequenceId)
-      ?.steps.find((step) => step.id === stepId)
-      ?.notes.find((note) => note.id === noteId)
+      .find((seq: SequenceInfo) => seq.programNumber === sequenceId)
+      ?.steps.find((step: StepInfo) => step.id === stepId)
+      ?.notes.find((note: NoteInfo) => note.id === noteId)
   )
 
   const updateNote = useVolcaStore((state) => state.updateNote)
@@ -30,32 +31,32 @@ const Note = ({ noteId, sequenceId, stepId, on, motionData }: NoteProps) => {
   const noteOptions = [...musicalNotes]
   const tiedNote = gateTime == 127
 
-  const handleTriggerChange = (event) => {
+  const handleTriggerChange = (event: any) => {
     const trigger = !!event.target.checked
     updateNote(sequenceId, stepId, noteId, { trigger })
   }
 
-  const handleNoteChange = (event) => {
+  const handleNoteChange = (event: any) => {
     const newNoteNumber = noteToMidi(`${event.target.value}${noteNumberToOctave(noteNumber)}`)
     updateNote(sequenceId, stepId, noteId, { note: [newNoteNumber, otherNoteValue] })
   }
 
-  const handleOctaveChange = (event) => {
+  const handleOctaveChange = (event: any) => {
     const newNoteNumber = noteToMidi(`${noteNumberToName(noteNumber)}${event.target.value}`)
     updateNote(sequenceId, stepId, noteId, { note: [newNoteNumber, otherNoteValue] })
   }
 
-  const handleVelocityChange = (event) => {
+  const handleVelocityChange = (event: any) => {
     updateNote(sequenceId, stepId, noteId, { velocity: parseInt(event.target.value, 10) })
   }
 
-  const handleTiedNoteChange = (event) => {
+  const handleTiedNoteChange = (event: any) => {
     const tiedNote = !!event.target.checked
     const newGateTime = tiedNote ? 127 : 100
     updateNote(sequenceId, stepId, noteId, { gateTime: newGateTime })
   }
 
-  const handleGateTimeChange = (event) => {
+  const handleGateTimeChange = (event: any) => {
     let newGateTime = parseInt(event.target.value, 10)
     updateNote(sequenceId, stepId, noteId, { gateTime: newGateTime })
   }
